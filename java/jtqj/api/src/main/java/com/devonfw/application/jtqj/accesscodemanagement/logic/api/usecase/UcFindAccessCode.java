c4e0be6cf55e9a41d427cd8915318885f91671af
Vicent Pérez <vicennt@gmail.com>
2020-11-20 11:57:28 +0100

    backend code

---
+++ java/jtqj/api/src/main/java/com/devonfw/application/jtqj/accesscodemanagement/logic/api/usecase/UcFindAccessCode.java
@@ -0,0 +1,37 @@
+package com.devonfw.application.jtqj.accesscodemanagement.logic.api.usecase;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.devonfw.application.jtqj.accesscodemanagement.logic.api.to.AccessCodeCto;
+import com.devonfw.application.jtqj.accesscodemanagement.logic.api.to.AccessCodeEto;
+import com.devonfw.application.jtqj.accesscodemanagement.logic.api.to.AccessCodeSearchCriteriaTo;
+
+public interface UcFindAccessCode {
+
+  /**
+   * Returns a composite AccessCode by its id 'id'.
+   *
+   * @param id The id 'id' of the AccessCode.
+   * @return The {@link AccessCodeCto} with id 'id'
+   */
+  AccessCodeCto findAccessCodeCto(long id);
+
+  /**
+   * Returns a paginated list of composite AccessCodes matching the search criteria.
+   *
+   * @param criteria the {@link AccessCodeSearchCriteriaTo}.
+   * @return the {@link List} of matching {@link AccessCodeCto}s.
+   */
+  Page<AccessCodeCto> findAccessCodeCtos(AccessCodeSearchCriteriaTo criteria);
+
+  /**
+   * Returns a paginated list of AccessCodeEto matching the search criteria.
+   *
+   * @param criteria the {@link AccessCodeSearchCriteriaTo}.
+   * @return the {@link List} of matching {@link AccessCodeEto}s.
+   */
+  Page<AccessCodeEto> findAccessCodeEtos(AccessCodeSearchCriteriaTo criteria);
+
+}
---
+++ java/jtqj/api/src/main/java/com/devonfw/application/jtqj/queuemanagement/logic/api/usecase/UcManageQueue.java
@@ -0,0 +1,40 @@
+package com.devonfw.application.jtqj.queuemanagement.logic.api.usecase;
+
+import com.devonfw.application.jtqj.queuemanagement.logic.api.to.QueueEto;
+
+/**
+ * Interface of UcManageQueue to centralize documentation and signatures of methods.
+ */
+public interface UcManageQueue {
+
+  /**
+   * Deletes a queue from the database by its id 'queueId'.
+   *
+   * @param queueId Id of the queue to delete
+   * @return boolean <code>true</code> if the queue can be deleted, <code>false</code> otherwise
+   */
+  boolean deleteQueue(long queueId);
+
+  /**
+   * Saves a queue and store it in the database.
+   *
+   * @param queue the {@link QueueEto} to create.
+   * @return the new {@link QueueEto} that has been saved with ID and version.
+   */
+  QueueEto saveQueue(QueueEto queue);
+
+  /**
+   * Decrease number of customers of the queue and update the queue.
+   *
+   * @param queueId id of the queue to decrease customer.
+   */
+  void decreaseQueueCustomer(long queueId);
+
+  /**
+   * Increase number of customers of the queue and update the queue.
+   *
+   * @param queueId id of the queue to increase customer.
+   */
+  void increaseQueueCustomer(long queueId);
+
+}
